7a58f611a2ed2799a5e00a03a6355597db62edec
Alex Bondarenko <bondarenko_al@rozetka.com.ua>
2017-04-26 18:52:21 +0300

    first template

---
+++ resources/views/layout/header.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>Isdox</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link href="css/app.css" rel="stylesheet" type="text/css" />
+<link rel="stylesheet" type="text/css" href="css/slider.css" />
+
+
+<script type="text/javascript" src="js/jquery-1.4.2.min.js"></script>
+<script type="text/javascript" src="js/script.js"></script>
+<script type="text/javascript" src="js/coin-slider.min.js"></script>
+</head>
+<body>
+<div class="main">
+  <div class="header">
+    <div class="header_resize">
+      <div class="menu_nav">
+        <ul>
+          <li class="active"><a href="index.html"><span>Home Page</span></a></li>
+          <li><a href="support.html"><span>Support</span></a></li>
+          <li><a href="about.html"><span>About Us</span></a></li>
+          <li><a href="blog.html"><span>Blog</span></a></li>
+          <li><a href="contact.html"><span>Contact Us</span></a></li>
+        </ul>
+      </div>
+      <div class="clr"></div>
+      <div class="logo">
+        <h1><a href="index.html"><span>Ukraine &ndash; UBC</span> <small>Стратегия решений иммиграционных и неиммиграционных вопросов</small></a></h1>
+      </div>
+      <div class="clr"></div>
+      <div class="slider">
+        <div id="coin-slider"> <a href="#"><img src="images/slide1.jpg" width="960" height="360" alt="" /><span>Aliquam fermentum arcu et velit fringillaonec ut felis ipsum. Maecenas lacinia orci cursuslacus sodales in posuere risus gravida. Lorem ipsum dolor sit amet, consectetng ipsum, in euismod velit fringilla vitae.</span></a> <a href="#"><img src="images/slide2.jpg" width="960" height="360" alt="" /><span>Tasellus ornare adipiscing ipsum, in euismod velit fringilla vitae. Aliquam fermentum arcu et velit fringilla vitae consequat lectus tempus. Donec at ante nec sem bibendum porttitor. Mauris eu accumsan justo. Suspendisse lacus ante.</span></a> <a href="#"><img src="images/slide3.jpg" width="960" height="360" alt="" /><span>Lorem ipsum dolor sit amet, consectetng ipsum, in euismod velit fringilla vitae. Aliquam fermentum arcu et velit fringillaonec ut felis ipsum. Maecenas lacinia orci cursuslacus sodales in posuere risus gravida.</span></a> </div>
+        <div class="clr"></div>
+      </div>
+      <div class="clr"></div>
+    </div>
+  </div>
