34199e4b3b4d37b6c3458784764d90178e180397
Alex Bondarenko <bondarenko_al@rozetka.com.ua>
2017-04-27 17:43:15 +0300

    separate layouts

---
+++ resources/views/layout/sidebar/downloads.blade.php
@@ -0,0 +1,24 @@
+<div class="sidebar-block block-100">
+    <div class="ribbon ribbon-blue ribbon-left"></div>
+    <div class="ribbon ribbon-blue ribbon-right"></div>
+    <div class="block-title bg-blue">
+        <h5>Анкеты для заполнения</h5>
+    </div>
+    <div class="block-content center">
+        <div class="block block-33">
+            <a href="#">
+                <img src="images/usa_flag.png">
+            </a>
+        </div>
+        <div class="block block-33">
+            <a href="#">
+                <img src="images/gb_flag.png">
+            </a>
+        </div>
+        <div class="block block-33">
+            <a href="#">
+                <img src="images/ca_flag.png">
+            </a>
+        </div>
+    </div>
+</div>
---
+++ resources/views/layout/sidebar/sidebar.blade.php
@@ -0,0 +1,6 @@
+<div class="sidebar">
+    <div class="inner-sidebar">
+        {!! view('layout/sidebar/contact-us') !!}
+        {!! view('layout/sidebar/downloads') !!}
+    </div>
+</div>
