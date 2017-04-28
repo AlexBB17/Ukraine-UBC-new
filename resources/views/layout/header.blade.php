43eb417c855a355466586f4b3fb4b40cd9a0091d
Alex Bondarenko <bondarenko_al@rozetka.com.ua>
2017-04-28 11:19:31 +0300

    viewport

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -3,6 +3,7 @@
     <title>UKraine-UBC</title>
     <link rel="stylesheet" type="text/css" href="css/app.css">
     <meta name="robots" content="noindex,nofollow">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <div class="wrapper">
 <div class="header">
