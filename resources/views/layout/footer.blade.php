6e18b9e23e0941fcb9a232b085d5afbecc20952e
Alex Bondarenko <bondarenko_al@rozetka.com.ua>
2017-04-28 17:03:09 +0300

    footer layout

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,3 +1,10 @@
-<div class="footer">Footer</div>
+<div class="footer">
+    <div class="content-wrapper footer-content">
+        <div class="row">
+            <div class="copyright">&copy; Ukraine-UBC 2017</div>
+            <div class="developer">Alex Bondarenko</div>
+        </div>
+    </div>
+</div>
 </div>
 </html>
