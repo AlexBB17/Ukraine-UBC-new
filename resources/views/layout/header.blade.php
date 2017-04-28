633131626f52db7143410b7501ece49b87190f6e
Alex Bondarenko <bondarenko_al@rozetka.com.ua>
2017-04-28 14:54:35 +0300

    adaptive menu

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -2,6 +2,7 @@
 <head>
     <title>UKraine-UBC</title>
     <link rel="stylesheet" type="text/css" href="css/app.css">
+    <script src="js/app.js" type="text/javascript"></script>
     <meta name="robots" content="noindex,nofollow">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
@@ -13,15 +14,15 @@
             <div class="logo-slogan font-b">Стратегия решений иммиграционных и неиммиграционных вопросов</div>
         </div>
 
-        <div class="nav-menu">
-            <div class="show-menu">...</div>
-            <ul>
+        <nav>
+            <a href="#" id="show-menu" class="show-menu">...</a>
+            <ul class="nav-menu js-nav-menu">
                 <li><a class="active" href="#">Главная</a></li>
                 <li><a href="#">США</a></li>
                 <li><a href="#">Великобритания</a></li>
                 <li><a href="#">Канада</a></li>
                 <li><a href="#">Контакты</a></li>
             </ul>
-        </div>
+        </nav>
     </div>
 </div>
